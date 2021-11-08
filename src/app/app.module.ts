d474ebc6bf6091cab2548e22f5d46a94937661e7
Jorge-ReyesC <jorge.reyescelpa@gmail.com>
2021-11-07 22:37:53 -0300

    Parte1-Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BasictableComponent } from './basictable/basictable.component';
+import { BlankComponent } from './blank/blank.component';
+import { Error404Component } from './error404/error404.component';
+import { FontawesomeComponent } from './fontawesome/fontawesome.component';
+import { MapgoogleComponent } from './mapgoogle/mapgoogle.component';
+//rutaS
+import { APP_ROUTING } from './app.routes';
+import { from } from 'rxjs';
+import { AsideComponent } from './aside/aside.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ProfileComponent,
+    BasictableComponent,
+    BlankComponent,
+    Error404Component,
+    FontawesomeComponent,
+    MapgoogleComponent,
+    AsideComponent,
+    TopbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    APP_ROUTING
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
